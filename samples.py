cd1394c802ac0f461cea3c97fcbb2d14e1314e53
Jakub Czarnik <139051501+JakubCzarnik@users.noreply.github.com>
2023-08-13 14:39:06 +0200

    Update samples.py

--- samples.py
+++ samples.py
@@ -15,9 +15,8 @@ y = Tensor(4, is_watched=True)
 with GradientTape():
    c = x*y
    d = c**2
-   e = d**3
 
-e.backward()
+d.backward()
 print(f"{x.grad = :.1f}") # d/dx [(x*y)**2] = 2(x*y)*y = 96.
 print(f"{y.grad = :.1f}") # d/dy [(x*y)**2] = 2(x*y)*x = 72.
 print(f"{c.grad = :.1f}") # d/dc [c**2]     = 2(c)     = 24.
