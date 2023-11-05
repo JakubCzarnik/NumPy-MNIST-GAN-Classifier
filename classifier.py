7dba8332a80cd37a91a7dc267d865f7b2fbf0e7b
Jakub Czarnik <czarnik.jakub3@gmail.com>
2023-11-05 19:40:42 +0100

    Confusion matrix implemented

--- classifier.py
+++ classifier.py
@@ -4,6 +4,10 @@ from learning.layer import Dense
 from mnist import MNIST
 import matplotlib.pyplot as plt
 import cv2
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import pandas as pd
+
 
 class Classifier:
    def __init__(self, input_size, output_size, learning_rate=0.005):
@@ -11,8 +15,8 @@ class Classifier:
       self.output_size = output_size
       self.learning_rate = learning_rate
 
-      self.layer1 = Dense(input_size, 392, activation="sigmoid")
-      self.layer2 = Dense(392, output_size, activation="sigmoid")
+      self.layer1 = Dense(input_size, 392, activation="sigmoid", kernel_initializer=np.random.uniform(-1e-2, 1e-2, (input_size, 392)))
+      self.layer2 = Dense(392, output_size, activation="sigmoid", kernel_initializer=np.random.uniform(-1e-2, 1e-2, (392, output_size)))
 
 
    def __call__(self, inputs):
@@ -84,22 +88,50 @@ def one_hot_encoder(data, n_classes):
    return one_hot_encoded
 
 
+def create_confusion_matrix(model, X_val, y_val, batch_size, epoch=0):
+   all_predictions = []
+
+   n_batches = X_val.shape[0]//batch_size
+   for i in range(n_batches):
+      val_x = X_val[i*batch_size: (i+1)*batch_size]
+
+      y_pred = model(val_x)
+      y_pred = np.argmax(y_pred, axis=1)
+      all_predictions.append(y_pred)
+   all_predictions = np.array(all_predictions)
+   all_predictions = all_predictions.reshape((-1))
+   cm = confusion_matrix(y_val, all_predictions)
+
+   df_cm = pd.DataFrame(cm, index=[i for i in "0123456789"],
+                     columns=[i for i in "0123456789"])
+
+   plt.figure(figsize=(5, 4))
+   sn.heatmap(df_cm, annot=True, fmt='g', cmap="Blues")
+   plt.xlabel("Predicted")
+   plt.ylabel("Real")
+   plt.title("Confusion Matrix")
+   plt.savefig(f"image_samples/{epoch}")
+   plt.close()
+
+
+
+
 # Load Data
 mndata = MNIST('MNIST')
-train_x, train_y = mndata.load_training()
-val_x, val_y = mndata.load_testing()
+X_train, y_train = mndata.load_training()
+X_val, y_val = mndata.load_testing()
 
-train_x = 2*np.array(train_x)/255 - 1
-train_y = np.array(train_y)
+X_train = 2*np.array(X_train)/255 - 1
+y_train = np.array(y_train)
 
-val_x = 2*np.array(val_x)/255 - 1
-val_y = np.array(val_y)
+X_val = 2*np.array(X_val)/255 - 1
+y_val = np.array(y_val)
 
-train_y = one_hot_encoder(train_y, 10)
+y_train = one_hot_encoder(y_train, 10)
 
 # Hyperparameters
 epochs = 1000
-lr = 0.01
+lr = 0.008
 batch_size = 50
 
 
@@ -107,14 +139,14 @@ model = Classifier(input_size=784, output_size=10, learning_rate=lr)
 
 
 # Training
-n_batches = train_x.shape[0] // batch_size
+n_batches = X_train.shape[0] // batch_size
 for epoch in range(epochs):
    print(f"Epoch {epoch}/{epochs}")
    t_loss = v_loss = t_acc = v_acc = 0
 
    for batch in range(0, n_batches):
-      batch_x = train_x[batch*batch_size: (batch+1)*batch_size]
-      batch_y = train_y[batch*batch_size: (batch+1)*batch_size]
+      batch_x = X_train[batch*batch_size: (batch+1)*batch_size]
+      batch_y = y_train[batch*batch_size: (batch+1)*batch_size]
 
       batch_x = Tensor(batch_x)
       with GradientTape():
@@ -138,11 +170,12 @@ for epoch in range(epochs):
 
       print(f"batch: {batch}/{n_batches}  {t_loss = :.3f}  {t_acc = :.2%}", end="\r")
 
-   if epoch==200:
-      model.learning_rate /= 2
+   if epoch==100:
+      model.learning_rate *= 0.7
    ### Visualize ###
    # show_from_file(model, file="test.png")
    # show_from_dataset(model, test_x)
+   create_confusion_matrix(model, X_val, y_val, batch_size, epoch)
      
-   v_loss, v_acc = evaluate(model, val_x, val_y, batch_size)
+   v_loss, v_acc = evaluate(model, X_val, y_val, batch_size)
    print(f"epoch: {epoch}  {t_loss = :.3f}  {t_acc = :.2%}  {v_loss = :.3f}  {v_acc = :.2%}   ")
